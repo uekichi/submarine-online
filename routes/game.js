02697328b8be5d6f2a7e63037661ffd31f9b6ff0
uekichi <uekichi@qc.commufa.jp>
2019-04-08 13:06:25 +0000

    修正

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../config');
+//var config = require('../config');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
